39545b01d0ee9a895f6789c10ad46dc4a7b58518
SuyashAnand2028 <suyashanand20@gmail.com>
2025-06-20 05:01:46 +0530

    all the files add

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -26,10 +26,10 @@ const Home = () => {
           </div>
         </div>
         <div className="more-cards">
-          <TitleCards title={"Blockbuster Movies"}/>
-          <TitleCards title={"Only On Netflix"}/>
-          <TitleCards title={"Upcoming"}/>
-          <TitleCards title={"Top Picks For You"}/>
+          <TitleCards title={"Blockbuster Movies"} category={"top_rated"}/>
+          <TitleCards title={"Only On Netflix"} category={"popular"}/>
+          <TitleCards title={"Upcoming"} category={"upcoming"}/>
+          <TitleCards title={"Top Picks For You"} category={"now_playing"}/>
         </div>
         <Footer/>
     </div>
