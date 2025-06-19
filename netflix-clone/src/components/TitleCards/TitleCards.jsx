9e17ad536c5849700a12e2a2c5d2a44182f2a46d
SuyashAnand2028 <suyashanand20@gmail.com>
2025-06-20 00:18:47 +0530

    hom page completed and login page almost ready

--- netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+const TitleCards = ({title,category}) => {
   return (
-    <div className='titlecards'>
-
+    <div className='title-cards'>
+      <h2>{title?title:"Popular On Netflix"}</h2>
+      <div className="card-list">
+        {cards_data.map((card,index)=>{
+            return <div className="card" key={index}>
+              <img src={card.image}/>
+              <p>{card.name}</p>
+            </div>
+        })}
+      </div>
     </div>
   )
 }
